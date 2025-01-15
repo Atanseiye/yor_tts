6d7f1ad7a664a26c9b807a7367e9ab18f32def6c
Kolade <koladeatanseiye@gmail.com>
2025-01-15 23:12:08 +0100

    extra

--- new/main.py
+++ new/main.py
@@ -1,13 +1,10 @@
+import pandas as pd
+from model_training import model
+from keras.models import Sequential
+from preprocessing import token_pad, cleaning
+from sklearn.model_selection import train_test_split
 
 
-yor_text = pd.read_csv('')
-eng_text = pd.read_csv('')
-
-yor_text = cleaning(yor_text)
-eng_text = cleaning(eng_text)
-
-yor_text = token_pad(yor_text)
-eng_text = token_pad(eng_text)
 
 # split data into train and test
 yor_train, yor_test, eng_train, eng_test = train_test_split(yor_text, eng_text, test_size=0.2, random_state=42)
