65122f73269942168eb193a8c6aeaf3a9a86a054
Kolade <koladeatanseiye@gmail.com>
2025-01-15 23:13:19 +0100

    extra

--- new/preprocessing.py
+++ new/preprocessing.py
@@ -1,20 +1,19 @@
+import re
+import string
+import nltk
+# import tensorflow as tf
+nltk.download('wordnet')
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 stemmer = nltk.SnowballStemmer('english')
 stopword = set(stopwords.words('yoruba'))
 
-def cleaning(text):
-    text = str(text).lower()
-    text = re.sub('\[.*?\]', '', text)
-    text = re.sub('https?://\S+/www\.\S+', '', text)
-    text = re.sub('<.*?>+', '', text)
-    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
-    text = [word for word in text.split(' ') if word not in stopword]
-    text = " ".join(text)
-    text = [stemmer.stem(word) for word in text.split(' ')]
-    text = " ".join(text)
 
-    return text
 
 
 
