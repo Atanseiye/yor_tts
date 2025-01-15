5e2ee6e71454d2ea23a0802b77a7a6fc418cd769
Kolade <koladeatanseiye@gmail.com>
2025-01-15 23:09:58 +0100

    extra

---
+++ new/secrets.py
@@ -0,0 +1,1 @@
+mongo_uri = 'mongodb+srv://kolade:bM0MkpVxYu9qMhAc@nkenne-cluster.zbsi9.mongodb.net/'
---
+++ new/update_packages.py
@@ -0,0 +1,18 @@
+import pkg_resources
+import subprocess
+import sys
+
+# List all installed packages
+installed_packages = [(d.project_name, d.version) for d in pkg_resources.working_set]
+
+# Function to update a package
+def update_package(package_name):
+    try:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", package_name])
+        print(f"Successfully updated {package_name}")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to update {package_name}: {e}")
+
+# Update all packages
+for package_name, _ in installed_packages:
+    update_package(package_name)
